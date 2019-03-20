0e3e908124a9ca1e594754010152c2214cf4f9ac
jeesang <jeesang@encoredtech.com>
2019-03-20 20:55:54 +0900

    cube_f1 : remove directory
    STM32Cube_FW_F1_V1.6.0

--- STM32Cube_FW_F1_V1.6.0/Projects/STM3210E_EVAL/Applications/IAP/IAP_Binary_Template/Utilities/axftobin.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo AXF to BIN file generation ...
-
-"C:\Program Files\Keil\ARM\ARMCC\bin\fromelf.exe" --bin --output ..\MDK-ARM\STM3210E_EVAL\STM3210E_EVAL_SysTick.bin ..\MDK-ARM\STM3210E_EVAL\STM3210E_EVAL.axf 
-
-echo AXF to BIN file generation completed.
-
